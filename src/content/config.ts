da6803e49e17ebe54019424f80ef16e16b5343bd
Christian Decker <50999401+cmdecker95@users.noreply.github.com>
2024-07-21 22:10:27 -0400

    Remove boilerplate

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,14 +2,13 @@ import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
   type: "content",
-  // Type-check frontmatter using a schema
   schema: z.object({
     title: z.string(),
     description: z.string(),
-    // Transform string to Date object
     pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
+    upDate: z.coerce.date().optional(),
+    image: z.string().optional(),
+    imageAlt: z.string().optional(),
   }),
 });
 
