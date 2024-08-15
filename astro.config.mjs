3dbd3e5fd42778d4264fd908dd79ad3546d9a03f
Christian Decker <decker_christian@bah.com>
2024-08-15 16:50:20 -0400

    Update site in Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://heartbeatcc.com",
+  site: "https://heartbeatcc.netlify.app",
   integrations: [mdx(), sitemap()],
 });
